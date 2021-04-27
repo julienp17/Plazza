776d2ed188da41bd726bd55063196a4c8c68f2c5
Julien PAUSE <julien.pause@epitech.eu>
2021-04-27 10:06:47 +0400

    feat: add usage

---
+++ inc/plazza.hpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2021
+** plazza [WSL: Ubuntu-20.04]
+** File description:
+** plazza
+*/
+
+#ifndef INC_PLAZZA_HPP_
+#define INC_PLAZZA_HPP_
+
+#define MY_EXIT_FAILURE 84
+#define USAGE \
+"Usage: plazza COOKING_TIME NB_COOKS STOCK_TIME\n" \
+"    COOKING_TIME: Positive number representing the multiplier for " \
+"the cooking time of the pizzas.\n" \
+"        NB_COOKS: Positive integer representing the number of cooks " \
+"per kitchen.\n" \
+"      STOCK_TIME: Positive integer representing the time in milliseconds " \
+"used by the kitchen stock to replace ingredients."
+
+
+#endif  // INC_PLAZZA_HPP_
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,16 @@
 ** main
 */
 
-int main(void) {
+#include <iostream>
+#include "plazza.hpp"
+
+int main(int ac, char **av) {
+    if (ac == 2 && std::string(av[1]) == "-h") {
+        std::cout << USAGE << std::endl;
+        return 0;
+    } else if (ac != 4) {
+        std::cerr << USAGE << std::endl;
+        return MY_EXIT_FAILURE;
+    }
     return 0;
 }
