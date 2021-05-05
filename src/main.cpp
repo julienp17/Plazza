d75fdd589e91ded948944758b5cc9837ced55956
Julien PAUSE <julien.pause@epitech.eu>
2021-05-05 16:21:01 +0400

    refactor(main): static functions and welcome message

--- src/main.cpp
+++ src/main.cpp
@@ -10,25 +10,23 @@
 #include "Reception.hpp"
 #include "Kitchen.hpp"
 
-static bool args_are_correct(int ac, char **av);
+static bool argsAreCorrect(int ac, char **av);
+plz::KitchenSettings getSettings(char **av);
 static void runReception(plz::Reception &reception);
 
 int main(int ac, char **av) {
     plz::Reception reception;
-    plz::KitchenSettings settings;
 
     if (ac == 2 && std::string(av[1]) == "-h")
         exitUsage(std::cout, 0);
-    else if (!args_are_correct(ac, av))
+    else if (!argsAreCorrect(ac, av))
         exitUsage(std::cerr, MY_EXIT_FAILURE);
-    settings.cookingMultiplier = getNumber<float>(av[1]);
-    settings.nbCooks = getNumber<size_t>(av[2]);
-    settings.restockTime = std::chrono::milliseconds(getNumber<size_t>(av[3]));
+    reception.setKitchenSettings(getSettings(av));
     runReception(reception);
     return 0;
 }
 
-static bool args_are_correct(int ac, char **av) {
+static bool argsAreCorrect(int ac, char **av) {
     return (
         ac == 4
         && isPositiveNumber<float>(av[1])
@@ -36,10 +34,21 @@ static bool args_are_correct(int ac, char **av) {
         && isPositiveNumber<size_t>(av[3]));
 }
 
+plz::KitchenSettings getSettings(char **av) {
+    plz::KitchenSettings settings;
+
+    settings.cookingMultiplier = getNumber<float>(av[1]);
+    settings.nbCooks = getNumber<size_t>(av[2]);
+    settings.restockTime = std::chrono::milliseconds(getNumber<size_t>(av[3]));
+    return settings;
+}
+
 static void runReception(plz::Reception &reception) {
     std::string orders;
 
+    std::cout << "Welcome to Plazza! The Pizzeria for everyone!" << std::endl;
     while (std::cin) {
+        std::cout << "Place your order here: ";
         getline(std::cin, orders);
         reception.placeOrders(orders);
     }
